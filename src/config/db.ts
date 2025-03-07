1a5df186bc10a5dc483a1eaffcda4f3ecf148d62
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-08 00:34:07 +0300

    hardcode the uri to the database for debigging

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     console.log("...");
-    await mongoose.connect(process.env.MONGO_URI as string);
+    await mongoose.connect(
+      "mongodb+srv://kaleabtadesse49:8l4hmRbk5gXn8hEY@ticketing-system.wzyzh.mongodb.net/ticketing-system?retryWrites=true&w=majority&appName=ticketing-system"
+    );
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
